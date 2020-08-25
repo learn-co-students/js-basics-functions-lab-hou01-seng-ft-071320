c07896db23925d20ff78fbfb3c4799f351b8a8ca
Tneebs <tedneben@gmail.com>
2020-08-24 19:43:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+    let result
+    if (blocks > 42) {
+        result = blocks - 42
+    } else {
+        result = 42 - blocks
+    }
+    return result
+}
+
+function distanceFromHqInFeet(feet){
+    let result
+    result = distanceFromHqInBlocks(feet) * 264
+    return result
+}
+
+function distanceTravelledInFeet(start_block, end_block){
+    let result
+    if (start_block > end_block){
+       result = (start_block - end_block) * 264
+    } else {
+        result = (end_block - start_block) * 264
+    }
+    return result
+}
+
+function calculatesFarePrice(start, destination){
+    const travelled = distanceTravelledInFeet(start,destination)
+    let result
+    if (travelled <= 400){
+        result = 0
+    } else if (travelled > 400 && travelled <= 2000){ 
+        result = .02 * (travelled - 400)
+    } else if (travelled > 2000 && travelled <= 2500){
+        result = 25
+    } else {
+        result = 'cannot travel that far'
+    }
+    return result
+}
